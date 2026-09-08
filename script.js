e2015672319cdc739c0ac16581f0faa228ee9f67
SamiPerala <sami.perala@hotmail.com>
2026-09-08 14:25:39 +0300

    Trying to draw hour numbers

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let test = "testing";
-
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
+const clockContainer = document.querySelector('.clock-container');
 
+let hasDrawnHours = false;
 
-
-console.log(test);
+function drawHours() {
+    if (!hasDrawnHours){
+     for (let i = 1; i <= 12; i++) {
+      clockContainer.innerHTML += `
+        <div class="number-container">
+        <p class="hour-number">${i}</p>"
+        </div>`
+        console.log(i)
+        if(i === 12) {
+            hasDrawnHours = !hasDrawnHours;
+        }
+    } 
+    } else
+        return;
+}
 
 function setTime() {
     const hour = new Date().getHours();
     const minute = new Date().getMinutes();
     const second = new Date().getSeconds();
-
-    console.log(hour)
     hourHand.style.transform = `rotate(${hour*30}deg)`;
     minuteHand.style.transform = `rotate(${minute*6}deg)`;
     secondHand.style.transform = `rotate(${second*6}deg)`;
@@ -23,6 +34,9 @@ function setTime() {
 
 
 function updateClock() {
+    do (hasDrawnHours) {
+        drawHours()
+    }
     setInterval(setTime, 1000)
 }
 
