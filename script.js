a107995a4fa23482055df93d6f10b242cbf1e7ca
SamiPerala <sami.perala@hotmail.com>
2026-09-08 17:46:23 +0300

    Fixed numbers braking the hand rotation

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
-const clockContainer = document.querySelector('.clock-container');
+const clockNumberContainer = document.querySelector('.clock-face-numbers-container');
 
 let hasDrawnHours = false;
 
-function drawHours() {
-    if (!hasDrawnHours){
-     for (let i = 1; i <= 12; i++) {
-      clockContainer.innerHTML += `
-        <div class="number-container">
-        <p class="hour-number">${i}</p>"
-        </div>`
-        console.log(i)
-        if(i === 12) {
-            hasDrawnHours = !hasDrawnHours;
-        }
-    } 
-    } else
-        return;
-}
+
 
 function setTime() {
+    let numberRot = 30;
+    if(!hasDrawnHours) {
+        for (let i = 1; i <= 12; i++) {
+      clockNumberContainer.innerHTML += `
+        <div class="number-container" style="transform: rotate(${numberRot}deg);">
+        <p class="hour-number" style="transform:  translate(0%, -440%) rotate(-${numberRot}deg);">${i}</p>
+        </div>`
+         numberRot += 30
+    } 
+       
+
+    hasDrawnHours = !hasDrawnHours;
+    }
     const hour = new Date().getHours();
     const minute = new Date().getMinutes();
     const second = new Date().getSeconds();
     hourHand.style.transform = `rotate(${hour*30}deg)`;
     minuteHand.style.transform = `rotate(${minute*6}deg)`;
     secondHand.style.transform = `rotate(${second*6}deg)`;
-    
-    
+    console.log(secondHand)
 }
 
 
 function updateClock() {
-    do (hasDrawnHours) {
-        drawHours()
-    }
     setInterval(setTime, 1000)
 }
 
